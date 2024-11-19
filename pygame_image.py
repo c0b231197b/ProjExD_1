951b8dc7f0341d9a5aa6de59f08614aed7985e20
23119 <c0b231197b@edu.teu.ac.jp>
2024-11-19 16:13:12 +0900

    練習7:ループ解消

--- pygame_image.py
+++ pygame_image.py
@@ -11,14 +11,18 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")
+    bg_img1 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
     x = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = tmr%800
-        screen.blit(bg_img, [-x, 0])
+        x = -(tmr%3200)
+        screen.blit(bg_img, [x, 0])
+        screen.blit(bg_img1, [(x+1600), 0])
+        screen.blit(bg_img, [(x+3200), 0])
+        screen.blit(bg_img1, [(x+4200), 0])
         screen.blit(kk_img,[300,200])
         pg.display.update()
         tmr += 1  
