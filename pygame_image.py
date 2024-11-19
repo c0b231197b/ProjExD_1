9d36b7cc828f6d4b9d1d2043a33d7f96bc89361c
23119 <c0b231197b@edu.teu.ac.jp>
2024-11-19 16:54:01 +0900

    演習:こうかとん常時移動

--- pygame_image.py
+++ pygame_image.py
@@ -17,18 +17,20 @@ def main():
     x = 0
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300,200
+    v = 1
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0, -1))
-        elif key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0, +1))
-        elif key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+1, 0))
-        elif key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1, 0))
+            kk_rct.move_ip((0, -(v)))
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0, (v)))
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((2*v, 0))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(((-v), 0))
+        kk_rct.move_ip(((-v),0))
         x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])
         screen.blit(bg_img1, [(x+1600), 0])
@@ -38,8 +40,6 @@ def main():
         pg.display.update()
         tmr += 1  
         clock.tick(200)
-
-
 
 if __name__ == "__main__":
     pg.init()
