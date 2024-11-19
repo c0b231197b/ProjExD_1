bb02ec072d52d6e4ba3130b08a76a520b889ebcc
23119 <c0b231197b@edu.teu.ac.jp>
2024-11-19 15:41:23 +0900

    練習３:こうかとんの登場

--- pygame_image.py
+++ pygame_image.py
@@ -18,11 +18,10 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [tmr_x, 0])
+        screen.blit(bg_img, [x, 0])
         screen.blit(kk_img,[300,200])
         pg.display.update()
-        tmr += 1     
-        tmr_x = tmr%800   
+        tmr += 1  
         clock.tick(10)
 
 
